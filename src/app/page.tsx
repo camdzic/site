f3b4de4d72c5cd643ed2d05964f73dc50c689cab
Aldin Čamdžić <acamdzic0@proton.me>
2024-07-10 14:24:46 +0200

    fix: responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,37 +6,40 @@ import { skills } from '@/data';
 export default function Home() {
   return (
     <MainLayout>
-      <div className="flex flex-col items-center sm:flex-row sm:items-start sm:space-x-6">
-        <div className="flex-grow">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
-            Hi, I&apos;m Aldin 👋
-          </h1>
-          <p className="mt-1 md:text-xl">Backend Developer</p>
-        </div>
-        <Avatar className="h-28 w-28">
-          <AvatarImage src="https://avatars.githubusercontent.com/u/111543914" />
-        </Avatar>
-      </div>
+      <section className="flex flex-col">
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
+              Hi, I&apos;m Aldin 👋
+            </h1>
+            <p className="mt-1 md:text-xl">Backend Developer</p>
+          </div>
 
-      <div className="space-y-0.5">
+          <Avatar className="h-24 w-24 sm:h-28 sm:w-28">
+            <AvatarImage src="https://avatars.githubusercontent.com/u/111543914" />
+          </Avatar>
+        </div>
+      </section>
+
+      <section className="space-y-0.5">
         <h2 className="text-xl font-bold">About</h2>
         <p className="text-sm text-muted-foreground">
           I&apos;m a sixteen-year-old student currently pursuing a degree in
           computer science. I have a passion for software development and enjoy
           creating new things.
         </p>
-      </div>
+      </section>
 
-      <div className="space-y-0.5">
+      <section className="space-y-0.5">
         <h2 className="text-xl font-bold">Skills</h2>
-        <div className="flex select-none gap-1">
+        <div className="flex select-none flex-wrap gap-1">
           {skills.map((skill) => (
             <Badge key={skill}>{skill}</Badge>
           ))}
         </div>
-      </div>
+      </section>
 
-      <div className="space-y-0.5">
+      <section className="space-y-0.5">
         <h2 className="text-xl font-bold">Contact</h2>
         <p className="text-sm text-muted-foreground">
           Want to chat? Just shoot me{' '}
@@ -48,7 +51,7 @@ export default function Home() {
           </a>{' '}
           and I&apos;ll get back to you as soon as possible.
         </p>
-      </div>
+      </section>
     </MainLayout>
   );
 }
